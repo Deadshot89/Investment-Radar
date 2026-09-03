94d0be76895c89ea9ee37fad0e82c74df5f58ee6
Deadshot89 <86518220+Deadshot89@users.noreply.github.com>
2026-09-03 13:31:05 +0200

    test: prefer tracked shares in portfolio analysis

--- android/app/src/test/java/de/tobias/investmentradar/PortfolioSnapshotValueTest.kt
+++ android/app/src/test/java/de/tobias/investmentradar/PortfolioSnapshotValueTest.kt
@@ -26,6 +26,42 @@ class PortfolioSnapshotValueTest {
     }
 
     @Test
+    fun portfolioAnalysisPrefersTrackedSharesAtCurrentPriceOverOldSnapshot() {
+        val item = InvestmentItem(
+            id = "meta",
+            type = "Aktie",
+            name = "Meta Platforms",
+            ticker = "META",
+            isin = "US30303M1027",
+            tradeRepublicName = "Meta Platforms",
+            status = "OK",
+            allocation = 0,
+            risk = 3,
+            price = 150.0,
+            priceEur = 150.0,
+            currency = "EUR",
+            fxRateToEur = 1.0,
+            fxSource = "test",
+            fxDelayed = false,
+            fxAsOf = null,
+            percentChange = null,
+            marketOpen = true,
+            dataSource = "test",
+            dataDelayed = false,
+            dataError = null
+        )
+        val position = PortfolioPosition(
+            itemId = "meta",
+            snapshotValueEur = 1675.88,
+            trackedShares = 2.0
+        )
+
+        val values = PortfolioAnalysis.values(listOf(item), mapOf("meta" to position), emptyList())
+
+        assertEquals(300.0, values.getValue("meta"), 0.001)
+    }
+
+    @Test
     fun portfolioMetricsIncludesSnapshotOnlyHoldingInWeights() {
         val positions = mapOf(
             "meta" to PortfolioPosition(itemId = "meta", snapshotValueEur = 1675.88),
