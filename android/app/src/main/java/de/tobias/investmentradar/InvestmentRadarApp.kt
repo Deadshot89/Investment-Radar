acca6db261a95cf05ee0044b92b56b7c406604c7
Deadshot89 <86518220+Deadshot89@users.noreply.github.com>
2026-09-11 21:30:13 +0200

    fix: initialize migrated live budget before advisor starts

--- android/app/src/main/java/de/tobias/investmentradar/InvestmentRadarApp.kt
+++ android/app/src/main/java/de/tobias/investmentradar/InvestmentRadarApp.kt
@@ -7,7 +7,7 @@ import com.google.firebase.FirebaseOptions
 class InvestmentRadarApp : Application() {
     override fun onCreate() {
         super.onCreate()
-        InvestmentBudgetStore.refreshRuntime(this)
+        InvestmentBudgetStore.ensureInitialized(this)
         FirebaseBootstrap.initialize(this)
         DailyAnalysisScheduler.schedule(this)
     }
